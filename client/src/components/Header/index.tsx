fa85a1493c20419e806aac12b3965efa9d4a1ed3
Xuan Li <xl3125@columbia.edu>
2023-10-13 15:26:01 -0400

    Project Page refine

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Header.tsx
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Button,
+  Box
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Header: React.FC = () => {
+  return (
+    <>
+      <div>
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: '#2d2d86', marginBottom: '8px' }}
+        >
+          <Toolbar>
+            <IconButton edge="start" color="inherit">
+              <img src="your-logo-url-here.png" alt="logo" height="40px" />
+            </IconButton>
+            <Box
+              sx={{
+                position: 'relative',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: '40%'
+              }}
+            >
+              <InputBase
+                placeholder="What can we help you buy today"
+                sx={{
+                  color: 'black',
+                  padding: '0 8px',
+                  paddingRight: '40px', // Increased padding to make space for icon
+                  width: '100%',
+                  backgroundColor: 'white',
+                  borderRadius: '4px',
+                  '&::placeholder': {
+                    color: 'black'
+                  }
+                }}
+              />
+              <SearchIcon
+                sx={{
+                  color: 'gray',
+                  position: 'absolute',
+                  right: '8px',
+                  top: '50%',
+                  transform: 'translateY(-50%)',
+                  cursor: 'pointer'
+                }}
+              />
+            </Box>
+            <Box>
+              <Button
+                color="inherit"
+                startIcon={<NotificationsNoneIcon />}
+                sx={{ textTransform: 'none', fontSize: '20px', mr: 2 }}
+              >
+                Deals
+              </Button>
+              <Button
+                color="inherit"
+                startIcon={<MenuIcon />}
+                sx={{ textTransform: 'none', fontSize: '20px', mr: 2 }}
+              >
+                Menu
+              </Button>
+              <Button
+                color="inherit"
+                startIcon={<AccountCircle />}
+                sx={{ textTransform: 'none', fontSize: '20px', mr: 2 }}
+              >
+                Account
+              </Button>
+            </Box>
+          </Toolbar>
+          <Box sx={{ backgroundColor: 'white', height: '3px' }} />
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <Button color="inherit" sx={{ textTransform: 'none' }}>
+              Top Deals
+            </Button>
+            <Button color="inherit" sx={{ textTransform: 'none' }}>
+              Electronics
+            </Button>
+            <Button color="inherit" sx={{ textTransform: 'none' }}>
+              Travel & Vacation
+            </Button>
+            <Button color="inherit" sx={{ textTransform: 'none' }}>
+              Automobile
+            </Button>
+            <Button color="inherit" sx={{ textTransform: 'none' }}>
+              More
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
