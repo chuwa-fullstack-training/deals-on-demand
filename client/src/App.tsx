5920944db0683265b12f0d7c8bc26a842a51ac11
larrywong98 <54010526+larrywong98@users.noreply.github.com>
2023-10-12 23:22:58 -0500

    Update App (#1)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import { createTheme, ThemeProvider } from '@mui/material';
 
+// Usage :
+// <Button sx={{ color: "primary.main" }}></Button>
+// <Button sx={{ color: (theme) => theme.palette.primary.main }}></Button>
+// <Button variant="contained" color="secondary"></Button>
+const theme = createTheme({
+  palette: {
+    // mode: 'dark',
+    primary: {
+      main: '#2a9461'
+    },
+    secondary: {
+      main: '#494c7d'
+    }
+  },
+  components: {}
+});
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>A Revamp of Deals on Demand</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <Router>
+          {/*header*/}
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+            </Route>
+          </Routes>
+          {/*footer*/}
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
