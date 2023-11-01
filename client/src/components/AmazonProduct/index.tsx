20ed32dcc4eda7180c4b49593d83228c19816cef
Larry Wang <larrywong9898@gmail.com>
2023-10-31 22:47:39 -0500

    fix amazon product style

--- client/src/components/AmazonProduct/index.tsx
+++ client/src/components/AmazonProduct/index.tsx
@@ -1,20 +1,28 @@
-import { Box, Button, Paper, Typography } from '@mui/material';
+import { Box, Button, Paper, Stack, Typography, useTheme } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { AmazonProduct as ItemType } from '@/types/amazon';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const AmazonProduct = ({ item }: { item: ItemType }) => {
   const navigate = useNavigate();
+  const { breakpoints } = useTheme();
+  const mediumSizeMatches = useMediaQuery(breakpoints.up('md'));
   const toDetails = () => {
     navigate('/product/amazon/' + item.id);
   };
   return (
     <Paper
-      sx={{ padding: { xs: '5px', md: '15px' }, margin: '10px 10px' }}
+      sx={{
+        width: { xs: 180, sm: 180, md: 250 },
+        height: { xs: 180, sm: 180, md: 250 },
+        padding: { xs: '5px', md: '15px' },
+        margin: { xs: '0 10px', md: '0 10px' }
+      }}
       elevation={3}
     >
       <Box
         sx={{
-          width: { xs: 100, md: 250 },
+          width: { xs: '100%', md: 250 },
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'start'
@@ -26,22 +34,18 @@ const AmazonProduct = ({ item }: { item: ItemType }) => {
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
-            margin: '5px 0'
+            margin: { xs: '0 0', md: '5px 0' }
           }}
         >
           <Box
             component="img"
-            sx={{ height: { xs: 50, md: 150 } }}
+            sx={{ height: { xs: 70, sm: 70, md: 150 } }}
             src={item.image1}
             alt=""
           />
         </Box>
         <Box>
-          <Typography
-            sx={{ fontSize: '10px', display: { xs: 'none', md: 'block' } }}
-          >
-            amazon
-          </Typography>
+          <Typography sx={{ fontSize: '10px' }}>amazon</Typography>
         </Box>
         <Box
           sx={{
@@ -75,17 +79,20 @@ const AmazonProduct = ({ item }: { item: ItemType }) => {
         <Box
           sx={{
             display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
+            flexDirection: { xs: 'column', sm: 'column', md: 'row' },
+            alignItems: { xs: 'start', sm: 'start', md: 'center' },
             justifyContent: 'space-between',
-            margin: '5px 0'
+            margin: { xs: '0 0', sm: '0 0', md: '5px 0' }
           }}
         >
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: '5px' }}>
+          <Stack
+            direction="row"
+            sx={{ gap: '5px', margin: { sm: '5px 0', md: '0 0' } }}
+          >
             {item.originalPrice !== item.currentPrice ? (
               <Typography
                 sx={{
-                  fontSize: '13px',
+                  fontSize: { xs: '11px', sm: '11px', md: '13px' },
                   color: 'green'
                 }}
               >
@@ -99,16 +106,25 @@ const AmazonProduct = ({ item }: { item: ItemType }) => {
             ) : (
               <Typography
                 sx={{
-                  fontSize: '13px',
+                  fontSize: { xs: '11px', sm: '11px', md: '13px' },
                   textDecoration: 'line-through'
                 }}
               >
                 ${item.originalPrice}
               </Typography>
             )}
-          </Box>
-          <Button onClick={toDetails} sx={{ fontSize: '10px' }}>
-            View Deals
+          </Stack>
+          <Button
+            variant="contained"
+            onClick={toDetails}
+            sx={{
+              // backgroundColor: 'primary.main',
+              fontSize: { xs: '8px', sm: '10px', md: '10px' },
+              width: { xs: '10px', sm: '10px', md: '100px' },
+              height: { xs: '20px', sm: '20px', md: '25px' }
+            }}
+          >
+            {mediumSizeMatches ? 'View Deals' : 'View'}
           </Button>
         </Box>
       </Box>
